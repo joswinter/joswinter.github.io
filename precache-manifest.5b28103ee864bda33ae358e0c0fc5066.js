5237471bc9db4b3397322e22f35ec8e6d6074d7c
Jos Winter <info@joswinter.nl>
2019-11-26 20:42:00 +0100

    Updates

---
+++ precache-manifest.5b28103ee864bda33ae358e0c0fc5066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae42e580e650b576c7fb515c92ac87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c81e3a53b61a8e61c3ee",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "deefaaf5341918d6f45d",
+    "url": "/static/js/2.08a9ee08.chunk.js"
+  },
+  {
+    "revision": "c81e3a53b61a8e61c3ee",
+    "url": "/static/js/main.64828938.chunk.js"
+  },
+  {
+    "revision": "b0ef10d24145b2dfdd19",
+    "url": "/static/js/runtime-main.6ec6d000.js"
+  }
+]);

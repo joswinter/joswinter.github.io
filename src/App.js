3cbd70ac54eb8ed63a3453903e3d85934c2f5a71
Jos Winter <info@joswinter.nl>
2019-11-27 20:25:58 +0100

    add experiences and duration

--- src/App.js
+++ src/App.js
@@ -4,14 +4,67 @@ import Experience from './Experience';
 class App extends React.Component {
   render() {
     const name = 'Jos Winter'
+    const experiences = [
+      {
+        company: 'Adyen',
+        function: 'Software Engineer',
+        startDate: '2018-09',
+        endDate: 'now',
+        skills: [
+          'java', 'sql'
+        ]
+      },
+      {
+        company: 'Adyen',
+        function: 'Graduate Intern',
+        startDate: '2017-11',
+        endDate: '2018-09',
+        skills: [
+          'java', 'sql'
+        ]
+      },
+      {
+        company: 'TU Delft',
+        function: 'Bachelor Graduate Intern',
+        startDate: '2016-03',
+        endDate: '2016-06',
+        skills: [
+          'python'
+        ]
+      },
+      {
+        company: 'TU Delft',
+        function: 'Research Intern',
+        startDate: '2014-11',
+        endDate: '2016-03',
+        skills: [
+          'haskell', 'f#'
+        ]
+      },
+      {
+        company: 'Synetic',
+        function: 'Web Developer',
+        startDate: '2014-06',
+        endDate: '2016-01',
+        skills: [
+          'php', 'js', 'sql', 'html'
+        ]
+      }
+    ]
     return (
       <div className="App">
         <div className="Name">{name}</div>
-        <Experience
-          company="Test Company 1"
-          startDate="01-2019"
-          endDate="02-2019"
-        />
+        {
+          experiences.map(experience => (
+          <Experience
+            company={experience.company}
+            function={experience.function}
+            startDate={experience.startDate}
+            endDate={experience.endDate}
+            skills={experience.skills}
+          />
+          ))
+        }
       </div>
     );
   }
--- src/Experience.js
+++ src/Experience.js
@@ -5,10 +5,39 @@ class Experience extends React.Component {
     return (
       <div className="Experience">
         <div className="Name">{this.props.company}</div>
+        <div className="Function">{this.props.function}</div>
+        <div className="Duration">{this.duration(this.props.startDate,
+                                                 this.props.endDate)}</div>
         <div className="StartDate">{this.props.startDate}</div>
         <div className="EndDate">{this.props.endDate}</div>
+        {this.props.skills.map(skill => (
+          <div className="Skills">{skill}</div>
+        ))}
       </div>
     );
+  }
+
+  duration(start, end) {
+    var startDate = new Date(start);
+    var endDate;
+    if (end == 'now') {
+      endDate = new Date();
+    } else {
+      endDate = new Date(end);
+    }
+    var years = endDate.getYear() - startDate.getYear();
+    var months = 1 + endDate.getMonth() - startDate.getMonth();
+
+    // correct date
+    if (months < 0) {
+      months += 12;
+      years -= 1;
+    }
+
+    if (years == 0) {
+      return months + ' months';
+    }
+    return years + ' years and ' + months + ' months';
   }
 }
 

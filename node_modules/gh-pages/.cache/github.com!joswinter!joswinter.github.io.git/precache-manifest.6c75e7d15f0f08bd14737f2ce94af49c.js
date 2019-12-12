f5a2878b8c7462ac6652ef04639331cabfacbbd7
Jos Winter <info@joswinter.nl>
2019-12-12 09:17:40 +0100

    Updates

---
+++ node_modules/gh-pages/.cache/github.com!joswinter!joswinter.github.io.git/precache-manifest.6c75e7d15f0f08bd14737f2ce94af49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d6844631ec47398183668f1fdfbadb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a030577e59686513907c",
+    "url": "/static/css/main.129050cd.chunk.css"
+  },
+  {
+    "revision": "ceab0ae61209ddb71d28",
+    "url": "/static/js/2.dee25166.chunk.js"
+  },
+  {
+    "revision": "a030577e59686513907c",
+    "url": "/static/js/main.5ab3391f.chunk.js"
+  },
+  {
+    "revision": "b0ef10d24145b2dfdd19",
+    "url": "/static/js/runtime-main.6ec6d000.js"
+  }
+]);

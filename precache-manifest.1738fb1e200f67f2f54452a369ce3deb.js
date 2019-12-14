bc591ab14619586b39cf93cbbf09c3fd7afabba8
Jos Winter <info@joswinter.nl>
2019-12-14 19:43:36 +0100

    Updates

---
+++ precache-manifest.1738fb1e200f67f2f54452a369ce3deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550720c13da11a4e724198b5c5ada67a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25581414c6df581a6b71",
+    "url": "/static/css/main.129050cd.chunk.css"
+  },
+  {
+    "revision": "ceab0ae61209ddb71d28",
+    "url": "/static/js/2.dee25166.chunk.js"
+  },
+  {
+    "revision": "25581414c6df581a6b71",
+    "url": "/static/js/main.5c2e03a8.chunk.js"
+  },
+  {
+    "revision": "b0ef10d24145b2dfdd19",
+    "url": "/static/js/runtime-main.6ec6d000.js"
+  }
+]);
